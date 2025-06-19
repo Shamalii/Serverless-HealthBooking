43965a8d9486d73851601863fcb23d7c55efba0e
Mostafa Shamali <97484612+Shamalii@users.noreply.github.com>
2025-06-20 02:38:14 +0300

    Update BookAppointment.vue

--- views/BookAppointment.vue
+++ views/BookAppointment.vue
@@ -62,7 +62,7 @@ export default {
         slot: this.selectedSlot
       };
 
-      fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments", {
+      fetch("https://430s2axyj6.execute-api.us-east-1.amazonaws.com/API/Appointments", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ body: JSON.stringify(payload) })

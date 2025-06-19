5db749d866ed5757438e425959935a164184505f
Mostafa Shamali <97484612+Shamalii@users.noreply.github.com>
2025-06-19 15:41:43 +0300

    Update BookAppointment.vue

--- views/BookAppointment.vue
+++ views/BookAppointment.vue
@@ -47,7 +47,7 @@ export default {
     };
   },
   mounted() {
-    fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/slots")
+    fetch("https://430s2axyj6.execute-api.us-east-1.amazonaws.com/API/Slots")
       .then(res => res.json())
       .then(data => {
         const parsed = JSON.parse(data.body);
